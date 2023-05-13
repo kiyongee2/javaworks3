cb91cce7cbb410119a4a99bceb45159eaae99352
kiyongee2 <kiyongee2@gmail.com>
2023-05-13 18:59:13 +0900

    java grammer

---
+++ ch01/src/hello/HelloJava.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("안녕~ Java");
+		System.out.println('K');  //문자 1개는 홑따옴표 사용
+		System.out.println(100);
+		System.out.println(4+5);
+		System.out.println(4+"5");
+		
+		System.out.println(true);
+	}
+
+}
---
+++ ch01/src/hello/Var.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class Var {
+
+	public static void main(String[] args) {
+		int grade;
+		grade = 3;
+		
+		String name;
+		name = "한지수";
+		
+		int schoolClass = 4;
+		
+		
+		System.out.println(grade + "학년 " + schoolClass + "반 " + name);
+	}
+
+}
