6b5b95d10d0bcee7f5daff053fd6b119fc73d73d
kiyongee2 <kiyongee2@gmail.com>
2023-05-26 07:54:07 +0900

    db connection2

--- DBConnection/src/db/person/PersonDAO.java
+++ DBConnection/src/db/person/PersonDAO.java
@@ -56,4 +56,69 @@ public class PersonDAO {
 		}
 		return personList;
 	}
+	
+	//특정한 1 사람 검색
+	public Person getPerson(String userId) {
+		Person person = new Person();
+		conn = JDBCUtil.getConnection();
+		String sql = "SELECT * FROM person WHERE userid = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				person.setUserId(rs.getString("userid"));
+				person.setUserPw(rs.getString("userpw"));
+				person.setName(rs.getString("name"));
+				person.setAge(rs.getInt("age"));		
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn, pstmt);
+		}
+		return person;
+	}
+	
+	//자료 삭제
+	public void deletePerson(String userId) {
+		conn = JDBCUtil.getConnection();
+		String sql = "DELETE FROM person WHERE userid = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn, pstmt);
+		}
+	}
+	
+	//자료 수정
+	public void updatePerson(Person person) {
+		conn = JDBCUtil.getConnection();
+		String sql = "UPDATE person SET userpw = ?, name = ?, age = ? WHERE userid = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, person.getUserPw());
+			pstmt.setString(2, person.getName());
+			pstmt.setInt(3, person.getAge());
+			pstmt.setString(4, person.getUserId());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn, pstmt);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
--- DBConnection/src/db/person/PersonMain.java
+++ DBConnection/src/db/person/PersonMain.java
@@ -6,13 +6,25 @@ public class PersonMain {
 
 	public static void main(String[] args) {
 		//Person 객체 생성
-		Person p1 = new Person("today", "today123", "투데이", 5);
+		Person p1 = new Person("test", "testing", "tester", 30);
 		
 		//PersonDAO 객체 생성
 		PersonDAO dao = new PersonDAO();
 		
 		//자료 삽입
 		//dao.insertPerson(p1);
+		
+		//자료 수정
+		//수정할 사람 1명 가져오기
+		Person tester = dao.getPerson("test");
+		tester.setName("테스터");
+		tester.setAge(100);
+		
+		//수정 실행
+		dao.updatePerson(tester);
+		
+		//자료 삭제
+		dao.deletePerson("test");
 		
 		//자료 조회
 		ArrayList<Person> personList = dao.getList();
@@ -27,6 +39,19 @@ public class PersonMain {
 			System.out.printf("아이디: %s, 비밀번호: %s, 이름: %s, 나이: %d\n", 
 					userid, userpw, name, age);
 		}
+		System.out.println("====================================================");
+		
+		//1명 검색해서 출력하기
+		Person person = dao.getPerson("today");
+		
+		String userid = person.getUserId();
+		String userpw = person.getUserPw();
+		String name = person.getName();
+		int age = person.getAge();
+		
+		System.out.printf("아이디: %s, 비밀번호: %s, 이름: %s, 나이: %d\n", 
+				userid, userpw, name, age);
+	
 	}
 
 }
